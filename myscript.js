c50f0f3a088c98405c61884cef2758feae3efc18
bartaadalbert <47637351+bartaadalbert@users.noreply.github.com>
2019-03-27 20:47:55 +0200

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,11 @@
+var date = new Date(); 
+var time = date.getDate(); 
+//if(time < 13) 
+//    alert('Доброе утро!'); 
+//else
+//    alert('Добрый вечер!'); 
+alert(time);
+
+console.log("sohodnisna date");
+console.log(time);
+document.write("Hello everybody");
